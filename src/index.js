20baf2e4e7cd5d6aee400b8c3f3936928fc4d327
chris <chrishethertonweb@gmail.com>
2022-10-10 15:34:05 +0200

    all done

--- src/index.js
+++ src/index.js
@@ -2,9 +2,15 @@ import React from "react"; //Main React.js library
 import ReactDOM from "react-dom/client"; //we use ReactDOM to render into the DOM
 
 // only update the value of this array
-const navlinkItems = [];
+const navlinkItems = [
+  'Link to google.com'
+  ,
+  'Link to facebook.com'
+  ,
+  'Link to amazon.com'
+];
 
-const content = <ul className="nav">{navlinkItems}</ul>;
+const content = <ul className="nav">{navlinkItems.map((item, idx) => <li className='nav-item' key={idx}><a className="nav-link" href="#">{item}</a></li>)}</ul>;
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 
